6cbcd6ab9b389876bcc215759efd5324215077f5
jeremy woo <kepungmoin@hotmail.com>
2020-01-13 20:49:57 +1300

    rename component

---
+++ belair_app/ClientApp/src/app/componentmould/mouldtop.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './mouldtop.component.html',
+})
+
+export class MouldTopComponent {
+
+
+  
+}
+
+
